2951301c6023c71d643cb753e9a66bd366af584e
Shayan Effati <shayan@effati.com>
2020-12-16 01:38:30 +0100

    𝒹𝒶𝑔 𝒻𝑒𝓂𝓉𝑜𝓃

--- 15/15.py
+++ 15/15.py
@@ -1,7 +1,15 @@
 def one_star():
+    return play_game(2020)
+
+
+def two_star():
+    return play_game(30000000)
+
+
+def play_game(n):
     mem = {f[n]: n + 1 for n in range(len(f) - 1)}
     prev = f[-1:][0]
-    for i in range(len(mem) + 2, 30000001):
+    for i in range(len(mem) + 2, n + 1):
         if prev not in mem:
             mem[prev] = i - 1
             prev = 0
@@ -15,3 +23,4 @@ def one_star():
 if __name__ == '__main__':
     f = [int(n) for n in "9,3,1,0,8,4".split(",")]
     print(one_star())
+    print(two_star())
